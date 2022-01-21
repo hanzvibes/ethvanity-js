39fd48cfe4bba9de392215e7dd4a41ccce6c6f76
Raihan Ramadhani <64344985+hanzvibes@users.noreply.github.com>
2022-01-22 03:36:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const {getVanityWallet} = require('./vanity.js');
 /**
  * getVanityWallet(input, isChecksum, isSuffix, minLength, callback)
  */
-getVanityWallet('00000000', false, false, 4, (message) => {
+getVanityWallet('00000000', false, false, 2, (message) => {
     console.log(message)
 })
