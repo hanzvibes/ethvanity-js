54e1ec3ca5bc8445f9aa175dc1281195aae08cbc
Raihan Ramadhani <64344985+hanzvibes@users.noreply.github.com>
2022-01-24 17:55:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const {getVanityWallet} = require('./vanity.js');
-/**
- * getVanityWallet(input, isChecksum, isSuffix, minLength, callback)
+
+// Loading Screen
+console.clear()
+console.log("\nGenerating Vanity Address....\n")
+
+/*
+ - input = 'A-F' / '0-9' chosen by the user
+ - isChecksum = 'true' Is a case-sensitive , 'false' is not a case-sensitive
+ - isSuffix = 'true' Is a suffix , 'false' is a prefix
+ - cb = Callback called after x attempts, or when an address if found
  */
 getVanityWallet('00000000', false, false, 2, (message) => {
     console.log(message)
